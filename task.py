6890c48abbb3e6c1f4ce7b0d706e2e7e09a6e295
M-Irina <irina24mad@gmail.com>
2024-11-30 16:30:23 +0900

    Add files via upload

---
+++ task.py
@@ -0,0 +1,7 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+average_grades = [[(5 + 3 + 3 + 5 + 4)/5], [(2 + 2 + 2 +3)/4], [(4 + 5 + 5 + 2)/4], [(4 + 4 + 3)/3], [(5 +5 +5 + 4 + 5)/5]]
+print(average_grades)
+students = {'Aaron':4.0, 'Bildo':2.25, 'Johnny':4.0, 'Khendrik':3.67, 'Steve':4.8}
+print(students)
+
